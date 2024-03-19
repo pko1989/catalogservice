238534111898312960d1446f1b3d31571654bcb5
Philipp Koessel <pkoessel@gmx.de>
2024-03-19 12:57:23 +0100

    blubb

---
+++ src/main/java/com/polarbookshop/catalogservice/web/BookControllerAdvice.java
@@ -0,0 +1,29 @@
+package com.polarbookshop.catalogservice.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.polarbookshop.catalogservice.domain.BookAlreadyExistsException;
+import com.polarbookshop.catalogservice.domain.BookNotFoundException;
+
+@RestControllerAdvice
+public class BookControllerAdvice {
+    @ExceptionHandler(BookNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String bookNotFoundHandler(BookNotFoundException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(BookAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    String bookAlreadyExistsHandler(BookAlreadyExistsException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    
+}
